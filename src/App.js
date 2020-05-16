1cdaa260c52aca8470c4636311ab42005f9d269d
HaaruunDhubat <dhubat.haaruun@gmail.com>
2020-05-16 17:57:04 -0400

    adding animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import cleandent from './images/cleandent_.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <h1>This is where the info will be</h1>
+            <div className="wrapper">
+                <div className="imgcontain">
+                    <img src={cleandent} />
+                </div>
+                <div className="textcontain">
+                    <h1 className="title">Check Back Soon!</h1>
+                </div>
+            </div>
         </div>
     );
 }
